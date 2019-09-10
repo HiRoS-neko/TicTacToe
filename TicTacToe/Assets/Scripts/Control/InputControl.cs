675e8adf406d0e01ba8ea295cdc61f5afeedbc1b
HiRoS-neko <hiros.neko@gmail.com>
2019-09-10 16:44:03 -0400

    Start of Parallax

---
+++ TicTacToe/Assets/Scripts/Control/InputControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Control
+{
+    public class InputControl : MonoBehaviour
+    {
+        public Action<Vector2> Rotation;
+
+        private void Update()
+        {
+            Rotation?.Invoke(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized);
+        }
+    }
+}
---
+++ TicTacToe/Assets/Scripts/Parallax/ParallaxControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Control;
+using UnityEngine;
+
+namespace Parallax
+{
+    [RequireComponent(typeof(Camera))]
+    public class ParallaxControl : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private Vector2 _cameraBounds;
+
+        [SerializeField] private InputControl _input;
+
+        private void OnEnable()
+        {
+            _cameraBounds = Vector2.one;
+            _camera = GetComponent<Camera>();
+            _input.Rotation += Rotation;
+        }
+
+        private void Rotation(Vector2 rot)
+        {
+            //rot is recieved as a normalized vector2
+            //to get camera position, it needs to be multiplied by the _cameraBounds
+            _camera.transform.localPosition = new Vector2(rot.x * _cameraBounds.x, rot.y * _cameraBounds.y);
+
+            //for the lens shift, it needs to be from 0 -> 1 instead of -1 -> 1
+            //var lensShift = (rot * 0.5f) + 0.5f * Vector2.one;
+            _camera.lensShift = rot * 0.25f;
+        }
+
+        private void OnDisable()
+        {
+            _input.Rotation -= Rotation;
+        }
+    }
+}
